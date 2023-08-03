9a14b3539f33cd7e7277c7ad31a4d852a414a07a
Hao Jiang ᛋ <22409868+Neo42@users.noreply.github.com>
2023-08-03 22:06:43 +0930

    feat: ✨ add question17

---
+++ question17.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+git branch branch1
+git branch branch2
+
+# main
+cp ./dir3/bar ./dir3/bar_copy
+git add . && git commit -m "Update main branch"
+
+# branch1
+git checkout branch1
+mv ./dir1/dir2/foo ./dir1/foo
+rmdir ./dir1/dir2
+touch ./newfile1
+git add . && git commit -m "Update branch1"
+
+# branch2
+git checkout branch2
+echo "Hello World" > ./dir1/dir2/foo
+mv ./dir1/dir2/foo ./dir1/dir2/foo_modified
+rm ./dir3/bar
+touch ./dir3/newfile2
+git add . && git commit -m "Update branch2"
